bf7fd797d5a5ce8ec7334d1eff90fa7c13a07d79
vladan345 <vladanm087@gmail.com>
2024-09-16 17:31:28 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-red-200 flex justify-center items-center">
+        <h1 className="text-center font-bold tracking-[-0.4px] text-[64px]">
+          Lab43
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
