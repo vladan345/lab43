e0f857a49165a0b08b4b36dcff9affbaf7e42b41
vladan345 <vladanm087@gmail.com>
2024-11-05 16:19:29 +0100

    update list

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,26 @@ import { Color } from "three";
 import Scene from "./Scene";
 
 function App() {
-  return (
-    <>
-      <div id="canvas-container" className="fixed top-0 left-0 w-full h-full">
-        <Canvas
-          camera={{ position: [0, 0, 20] }}
-          scene={{ background: new Color("#102020") }}
-        >
-          <Scene />
-        </Canvas>
-      </div>
-      {/* <div className="h-screen relative z-[1] flex justify-center items-center">
+   return (
+      <>
+         <div
+            id="canvas-container"
+            className="fixed top-0 left-0 w-full h-full"
+         >
+            <Canvas
+               camera={{ position: [0, 0, 20] }}
+               scene={{ background: new Color("#102020") }}
+            >
+               <Scene />
+            </Canvas>
+         </div>
+         {/* <div className="h-screen relative z-[1] flex justify-center items-center">
         <h1 className="text-center font-bold tracking-[-0.4px] text-[64px] text-[#fff]">
           Lab43
         </h1>
       </div> */}
-    </>
-  );
+      </>
+   );
 }
 
 export default App;
--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,8 +1,8 @@
 import {
-  ScrollControls,
-  useScroll,
-  useVideoTexture,
-  useTexture,
+   ScrollControls,
+   useScroll,
+   useVideoTexture,
+   useTexture,
 } from "@react-three/drei";
 import React, { useRef, Suspense } from "react";
 import { useFrame } from "@react-three/fiber";
@@ -13,94 +13,100 @@ import fragment from "./shaders/fragment.frag";
 import vertex from "./shaders/vertex.vert";
 
 export default function Scene() {
-  return (
-    <>
-      <ScrollControls infinite pages={20} damping={0.5}>
-        <Rig scale={window.innerWidth < 900 ? 0.6 : 1} />
-        <ambientLight args={["#fff", 3]} />
-        <directionalLight args={["#fff", 3]} />
-      </ScrollControls>
-    </>
-  );
+   return (
+      <>
+         <ScrollControls infinite pages={10} damping={0.5}>
+            <Rig scale={window.innerWidth < 900 ? 0.6 : 1} />
+            <ambientLight args={["#fff", 3]} />
+            <directionalLight args={["#fff", 3]} />
+         </ScrollControls>
+      </>
+   );
 }
 function Rig(props) {
-  const rig = useRef();
-  const scroll = useScroll();
+   const rig = useRef();
+   const scroll = useScroll();
 
-  useFrame((state, delta) => {
-    rig.current.rotation.y = -scroll.offset * (Math.PI * 2); // Rotate contents
-    state.events.update(); // Raycasts every frame rather than on pointer-move
-    easing.damp3(rig.current.position, [state.pointer.x * 0.25, 0, 0]);
-    easing.damp3(
-      state.camera.position,
-      [0, -state.pointer.y * 0.25, 4],
-      0.3,
-      delta
-    ); // Move camera
+   useFrame((state, delta) => {
+      rig.current.rotation.y = -scroll.offset * (Math.PI * 2); // Rotate contents
+      state.events.update(); // Raycasts every frame rather than on pointer-move
+      easing.damp3(rig.current.position, [state.pointer.x * 0.25, 0, 0]);
+      easing.damp3(
+         state.camera.position,
+         [0, -state.pointer.y * 0.25, 2],
+         0.3,
+         delta
+      ); // Move camera
 
-    state.camera.lookAt(0, 0, 0); // Look at center
-  });
-  return (
-    <group ref={rig} {...props}>
-      <Carousel />
-    </group>
-  );
+      state.camera.lookAt(0, 0, 0); // Look at center
+   });
+   return (
+      <group ref={rig} {...props}>
+         <Carousel />
+      </group>
+   );
 }
 
-function Carousel({ radius = 3, count = 13 }) {
-  return (
-    <group rotation={[0, 0, -0.1]}>
-      {Array.from({ length: count }, (_, i) => {
-        return (
-          <mesh
-            key={i}
-            position={[
-              Math.sin((i / count) * Math.PI * 2) * radius,
-              0,
-              Math.cos((i / count) * Math.PI * 2) * radius,
-            ]}
-            rotation={[0, (i / count) * Math.PI * 2, 0]}
-          >
-            <planeGeometry />
-            <Suspense fallback={<FallbackMaterial url="/test.jpg" />}>
-              <VideoMaterial url={`/temp${i + 1}.mp4`} />
-            </Suspense>
-          </mesh>
-        );
-      })}
-    </group>
-  );
+function Carousel({ radius = 0.5, count = 3 }) {
+   const handleClick = () => {
+      window.open("https://cofecofe.vercel.app/", "_blank");
+   };
+   return (
+      <group rotation={[0, 0, -0.1]}>
+         {Array.from({ length: count }, (_, i) => {
+            return (
+               <mesh
+                  onClick={handleClick}
+                  key={i}
+                  position={[
+                     Math.sin((i / count) * Math.PI * 2) * radius,
+                     0,
+                     Math.cos((i / count) * Math.PI * 2) * radius,
+                  ]}
+                  rotation={[0, (i / count) * Math.PI * 2, 0]}
+               >
+                  <planeGeometry />
+                  <Suspense fallback={<FallbackMaterial url="/cofe.png" />}>
+                     <VideoMaterial url={`/temp${i + 1}.mp4`} />
+                  </Suspense>
+               </mesh>
+            );
+         })}
+      </group>
+   );
 }
 
 function VideoMaterial({ url }) {
-  const ref = useRef(null);
-  useFrame(({ pointer, clock }) => {
-    ref.current.uniforms.u_time.value = clock.getElapsedTime();
-    ref.current.uniforms.u_mouse.value = [pointer.x, pointer.y];
-  });
-  const texture = useVideoTexture(url, {
-    start: false,
-    muted: true,
-    loop: true,
-    preload: "auto",
-  });
-  const uniforms = {
-    u_time: { value: 1 },
-    u_resolution: { value: [window.innerWidth, window.innerHeight] },
-    u_mouse: { value: [0, 0] },
-  };
-  return (
-    <shaderMaterial
-      side={DoubleSide}
-      uniforms={uniforms}
-      vertexShader={vertex}
-      fragmentShader={fragment}
-      ref={ref}
-    />
-  );
+   const ref = useRef(null);
+   useFrame(({ pointer, clock }) => {
+      ref.current.uniforms.u_time.value = clock.getElapsedTime();
+      ref.current.uniforms.u_mouse.value = [pointer.x, pointer.y];
+   });
+   const texture = useVideoTexture(url, {
+      start: false,
+      muted: true,
+      loop: true,
+      preload: "auto",
+   });
+   const uniforms = {
+      u_time: { value: 1 },
+      u_resolution: { value: [window.innerWidth, window.innerHeight] },
+      u_mouse: { value: [0, 0] },
+   };
+   return (
+      <shaderMaterial
+         side={DoubleSide}
+         uniforms={uniforms}
+         vertexShader={vertex}
+         fragmentShader={fragment}
+         ref={ref}
+      />
+   );
 }
 
 function FallbackMaterial({ url }) {
-  const texture = useTexture(url);
-  return <meshBasicMaterial map={texture} toneMapped={false} />;
+   const texture = useTexture(url);
+   return (
+      <meshBasicMaterial side={DoubleSide} map={texture} toneMapped={false} />
+   );
 }
